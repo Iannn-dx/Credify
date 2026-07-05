16f25d374e4c95e8f39cd9b8ee04face257c0aac
Ian <tevesiankhristopher@gmail.com>
2026-07-05 20:58:18 +0800

    create modal for credentials

--- resources/views/user/credentials/create.blade.php
+++ resources/views/user/credentials/create.blade.php
@@ -1,7 +1,7 @@
 <div
     x-show="open"
     x-cloak
-    @click.self="open = false"
+    @click.away="open = false"
     class="fixed inset-0 z-50 flex items-center justify-center bg-black/50">
 
     <div
@@ -19,7 +19,6 @@
                 </p>
             </div>
 
-            <!-- Close button -->
             <button
                 type="button"
                 @click="open = false"
@@ -35,6 +34,20 @@
             </button>
 
         </div>
+
+        <form action="" method="POST" enctype="multipart/form-data">
+          @csrf
+          <div class="p-6 space-y-5 bg-white">
+            {{-- doc titke --}}
+            <div>
+              <x-form-label for="title"/>
+                Document Title
+              <x-form-input type="text" name="title" id="doc_title" placeholder="Document Title" required autofocus class="block w-full px-3 py-2.5 border border-gray-200 rounded-lg text-sm placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-indigo-500/20 focus:border-indigo-500 transition-all bg-gray-50/50 focus:bg-white"/>
+            </div>
+
+            
+          </div>
+        </form>
 
     </div>
 </div>
